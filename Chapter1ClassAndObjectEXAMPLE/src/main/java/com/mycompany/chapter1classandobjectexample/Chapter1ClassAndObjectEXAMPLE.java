70cf43b6c8f55131888e56a59a8d28b05c61b53f
WannAisyah <141194044+Vaesya@users.noreply.github.com>
2023-08-09 11:11:47 +0800

    Add files via upload

---
+++ Chapter1ClassAndObjectEXAMPLE/src/main/java/com/mycompany/chapter1classandobjectexample/Chapter1ClassAndObjectEXAMPLE.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.chapter1classandobjectexample;
+
+/**
+ *
+ * @author Gigabyte
+ */
+public class Chapter1ClassAndObjectEXAMPLE {
+    
+    int x=5;
+    
+    public static void main(String[] args) {
+        Chapter1ClassAndObjectEXAMPLE MyObj = new Chapter1ClassAndObjectEXAMPLE();
+        System.out.println(MyObj.x);
+    }
+}
