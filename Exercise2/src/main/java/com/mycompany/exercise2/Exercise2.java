8b7303cf2a9866c83f2cb2dad36ab4a3266e36fe
WannAisyah <141194044+Vaesya@users.noreply.github.com>
2023-11-14 22:13:33 +0800

    Add files via upload

---
+++ Exercise2/src/main/java/com/mycompany/exercise2/Exercise2.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.exercise2;
+
+public class Exercise2 {
+
+    public static void main(String[] args) {
+        makanan();
+    }
+    
+    public static void makanan(){
+        System.out.println("I want to eat Mee Ladna");
+    }
+}
